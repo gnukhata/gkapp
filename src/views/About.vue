6a952e43138bfa9c194befb0ad4d0adba94b4823
Sai Karthik <kskarthik@disroot.org>
2020-12-06 22:44:14 +0530

    add buttons for website & source in about view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,26 @@
 <template>
     <div class="container is-fluid mt-5 mb-5">
         <div class="has-text-centered">
-            <img src="img/icons/icon.svg" alt="GNUKhata Logo">
-            <h1 class="mt-2 title is-4 has-text-centered">About GNUKhata</h1>
+            <img src="img/icons/icon.svg" alt="GNUKhata Logo" width=80px>
+            <h1 class="mt-2 title is-3 has-text-centered">GNUKhata</h1>
         </div>
         <div class="content mt-5">
-             <div class="has-text-centered mb-3 "><i>A Community Diven Open Source Accounting Software.</i></div>
-            <p class="has-text-justfied">
-                Founded and Developed by Digital Freedom Foundation under the leadership of Mr. Krishnakant Mane
-                Currently supported by Accion Labs Pvt. Ltd as a social contribution to the Open Source Community
-                <br>
-                <br>
-                <b>Website: </b><a href="https://gnukhata.in" target="_blank">https://gnukhata.in</a><br>
-                <b>Source Code: </b><a href="https://gitlab.com/gnukhata">https://gitlab.com/gnukhata</a>
+             <div class="subtitle is-5 has-text-centered mb-5">
+                 <i>A Community Driven Open Source Accounting Software</i>
+             </div>
+            <p class="subtitle is-size-5 has-text-justfied">
+                Founded and Developed by Digital Freedom Foundation under the leadership of Mr. Krishnakant Mane.
+                Currently supported by <a class='has-text-weight-bold' href="https://accionlabs.com" target="_blank" rel="noopener noreferrer">Accion Labs Pvt. Ltd</a>
+                as a social contribution to the Open Source Community.
             </p>
+            <div class="mt-5 has-text-centered">
+                <a class="mx-1" href="https://gnukhata.in" target="_blank">
+                    <button class="button is-primary"><span class="mdi mdi-earth"></span>Website</button>
+                </a>
+                <a href="https://gitlab.com/gnukhata" target="_blank">
+                    <button class="button is-primary"><span class="mdi mdi-source-repository"></span>Source Code</button>
+                </a>
+            </div>
         </div>
     </div>
 </template>
