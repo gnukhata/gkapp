2ded75f874ef1657428b92c3bd89666f2e0028ec
Sai Karthik <kskarthik@disroot.org>
2020-12-18 21:43:02 +0530

    don't hash output file names

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  productionSourceMap: false,
-  publicPath: process.env.DEPLOY_ENV === 'GL_PAGES'
-    ? '/gkapp/'
-    : '/',
-
+  productionSourceMap: false, // Don't generate js source maps
+  filenameHashing: false, // Don't hash output file names
+  publicPath: process.env.DEPLOY_ENV === 'GL_PAGES' ? '/gkapp/' : '/',
   // Remove moment.js from chart.js
   // https://www.chartjs.org/docs/latest/getting-started/integration.html#bundlers-webpack-rollup-etc
   configureWebpack: config => {
