0df1e191229683ae27197470e19f04016437f135
Sai Karthik <kskarthik@disroot.org>
2020-12-25 22:33:32 +0530

    TitleBar.vue: hide back to dashboard button in dashboard vue

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -10,7 +10,7 @@
       </div>
       <div class="level-right">
         <div class="level-item">
-          <div class="buttons is-right">
+          <div v-if="$route.path !== '/'" class="buttons is-right">
             <router-link to="/">
             <button class="button is-dark">
               <b-icon icon="arrow-left-circle" custom-size="default"/>
