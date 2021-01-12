6b3464649033478c6be8d46fe2a68bc5df7f952c
Sai Karthik <kskarthik@disroot.org>
2021-01-12 17:35:47 +0530

    update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about mt-5 text-muted">
-    <h1>About GNUKhata</h1>
+<div class="container">
+  <b-img fluid center width="90" src="img/icons/manifest-icon-192.png"></b-img>
+  <div class="text-center mt-2">
+    <h4 class="font-italic">GNUKhata is A Community Driven Free &amp; Open Source Accounting Software</h4>
+    <p class="text-muted">
+    Founded and Developed by Digital Freedom Foundation under the leadership of Mr. Krishnakant Mane.
+    Currently supported by <a class='has-text-weight-bold' href="https://accionlabs.com" target="_blank" rel="noopener noreferrer">Accion Labs Pvt. Ltd</a>
+    as a social contribution to the Open Source Community.
+   </p>
+<div class="mt-5 has-text-centered">
+    <a class="mx-1" href="https://gnukhata.in" target="_blank">
+        <b-button variant="primary">
+          <b-icon icon="globe"></b-icon> Website</b-button>
+    </a>
+    <a href="https://gitlab.com/gnukhata" target="_blank">
+        <b-button variant="primary">
+          <b-icon icon="code-square"></b-icon> Source Code</b-button>
+    </a>
+    <br>
+    <img class='mt-5' src="img/agpl3.png" alt="AGPLv3 License">
+</div>
+
   </div>
+</div>
 </template>
