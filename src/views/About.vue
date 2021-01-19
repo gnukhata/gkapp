79ddc32ff89cb9d8393d9929dea253bdbe22a933
SaiKarthik <kskarthik@disroot.org>
2021-01-19 16:35:03 +0530

    show app version in about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,11 @@
 <template>
 <div class="container">
   <b-img fluid center width="90" src="img/icons/manifest-icon-192.png"></b-img>
+    <div class="text-center"><code>v{{ver}}</code></div>
+
   <div class="text-center mt-2">
     <h4 class="font-italic">GNUKhata is A Community Driven Free &amp; Open Source Accounting Software</h4>
-    <p class="text-muted">
+    <p class="text-muted text-left">
     Founded and Developed by Digital Freedom Foundation under the leadership of Mr. Krishnakant Mane.
     Currently supported by <a class='has-text-weight-bold' href="https://accionlabs.com" target="_blank" rel="noopener noreferrer">Accion Labs Pvt. Ltd</a>
     as a social contribution to the Open Source Community.
@@ -18,9 +20,20 @@
           <b-icon icon="code-square"></b-icon> Source Code</b-button>
     </a>
     <br>
-    <img class='mt-5' src="img/agpl3.png" alt="AGPLv3 License">
+    <img class='mt-3' src="img/agpl3.png" alt="AGPLv3 License">
 </div>
 
   </div>
 </div>
 </template>
+<script>
+import {version} from '../../package'
+export default {
+  name: 'About',
+  data() {
+    return {
+      ver: version
+    }
+  }
+}
+</script>
