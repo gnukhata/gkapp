6436d68faea92680de022cf4c3c4819f862c8e8f
SaiKarthik <kskarthik@disroot.org>
2021-01-19 15:04:54 +0530

    automatically apply colors based on project version

--- src/components/ColorBar.vue
+++ src/components/ColorBar.vue
@@ -1,36 +1,43 @@
 <template>
     <div class="d-flex flex-row justify-content-left">
-        <div class="m-1" v-for="(style, index) in version" :key="index" :style="style"></div>
+        <div class="m-1" v-for="(style, index) in colorVersion" :key="index" :style="style"></div>
     </div>
 </template>
 
 <script>
+import {version} from '../../package'
 export default {
     name: 'ColorBar',
     data() {
         return {
       // color codes for numbers
-        number: {
-            zero: 'violet',
-            one: 'Indigo',
-            two: 'blue',
-            three: 'green',
-            four: 'yellow',
-            five: 'orange',
-            six: 'red',
-            seven: 'black',
-            eight: 'maroon',
-            nine: 'coral'
-            },
-        version: []
+        // number: {
+        //     zero: 'violet',
+        //     one: 'Indigo',
+        //     two: 'blue',
+        //     three: 'green',
+        //     four: 'yellow',
+        //     five: 'orange',
+        //     six: 'red',
+        //     seven: 'black',
+        //     eight: 'maroon',
+        //     nine: 'coral'
+        //     },
+            colors: [
+            'violet','Indigo','blue',
+            'green', 'yellow', 'orange', 
+            'red','black','maroon','coral'
+            ],
+        colorVersion: []
         }
     },
     methods: {
         ver() {
-            this.version = [
-                `background-color: ${this.number.zero}; height: 3px; width: 55%`,
-                `background-color: ${this.number.one}; height: 3px; width: 35%`,
-                `background-color: ${this.number.zero}; height: 3px; width: 15%`
+            const projectVersion = version.split('.')
+            this.colorVersion = [
+                `background-color: ${this.colors[projectVersion[0]]}; height: 3px; width: 55%`,
+                `background-color: ${this.colors[projectVersion[1]]}; height: 3px; width: 35%`,
+                `background-color: ${this.colors[projectVersion[2]]}; height: 3px; width: 15%`
             ]
         }
     },
