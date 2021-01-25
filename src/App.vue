452338794a04aac6e67a41bfb89c3cdca4d672da
SaiKarthik <kskarthik@disroot.org>
2021-01-25 17:08:25 +0530

    Notify user about installing latest version of app

--- src/App.vue
+++ src/App.vue
@@ -4,61 +4,88 @@
       <!--navbar-->
       <b-navbar toggleable="lg" type="light" variant="light">
         <b-navbar-brand>
-          <img src="img/gk.png" width="30" height="30" class="d-inline-block align-top" alt="GNUKhata Logo">
+          <img
+            src="img/gk.png"
+            width="30"
+            height="30"
+            class="d-inline-block align-top"
+            alt="GNUKhata Logo"
+          />
           GNUKhata
         </b-navbar-brand>
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav v-if="$router.currentRoute.path !== '/' && $router.currentRoute.path !== '/createorg'">
-            <b-nav-item :to="{ name: 'Dashboard' }" :active="activeNav === 'Dashboard'">Dashboard</b-nav-item>
-            <b-nav-item :to="{ name: 'About' }" :active="activeNav === 'About'">About</b-nav-item>
-            <b-nav-item :to="{ name: 'Login' }" :active="activeNav === 'Login'">Login</b-nav-item>
+          <b-navbar-nav
+            v-if="
+              $router.currentRoute.path !== '/' &&
+              $router.currentRoute.path !== '/createorg'
+            "
+          >
+            <b-nav-item
+              :to="{ name: 'Dashboard' }"
+              :active="activeNav === 'Dashboard'"
+              >Dashboard</b-nav-item
+            >
+            <b-nav-item :to="{ name: 'About' }" :active="activeNav === 'About'"
+              >About</b-nav-item
+            >
+            <b-nav-item :to="{ name: 'Login' }" :active="activeNav === 'Login'"
+              >Login</b-nav-item
+            >
           </b-navbar-nav>
           <b-navbar-nav class="ml-auto">
-            <b-nav-item-dropdown v-if="$router.currentRoute.path !== '/' && $router.currentRoute.path !== '/createorg'" right>
+            <b-nav-item-dropdown
+              v-if="
+                $router.currentRoute.path !== '/' &&
+                $router.currentRoute.path !== '/createorg'
+              "
+              right
+            >
               <!-- Using 'button-content' slot -->
               <template #button-content>
                 <!-- <em>{{userName}} </em> -->
-                <b-button variant="primary">{{userName}}</b-button>
+                <b-button variant="primary">{{ userName }}</b-button>
               </template>
               <!-- <b-dropdown-item href="#">Profile</b-dropdown-item> -->
-              <b-dropdown-item @click="logOut" href="#">Log Out</b-dropdown-item>
+              <b-dropdown-item @click="logOut" href="#"
+                >Log Out</b-dropdown-item
+              >
             </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
+          </b-navbar-nav>
+        </b-collapse>
       </b-navbar>
-     <color-bar></color-bar>
+      <color-bar></color-bar>
     </header>
     <main role="main">
-      <router-view/>
+      <router-view />
     </main>
   </div>
 </template>
 <script>
-import { mapState } from 'vuex'
-import ColorBar from '@/components/ColorBar.vue'
+import { mapState } from "vuex";
+import ColorBar from "@/components/ColorBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: { ColorBar },
   computed: {
     activeNav: (self) => self.$route.name,
-    ...mapState(['userName'])
+    ...mapState(["userName"]),
   },
   methods: {
     /**
      * Logout the user
      */
     logOut() {
-      localStorage.clear() // clear localStorage
-      this.$router.push('/') // redirect to login page
+      localStorage.clear(); // clear localStorage
+      this.$router.push("/"); // redirect to login page
       // alert the user on logout
       this.$bvToast.toast(`Logged out succesfully`, {
-        title: 'Logout',
+        title: "Logout",
         solid: true,
-        variant: 'success'
-        })
-    }
+        variant: "success",
+      });
+    },
   },
   created() {
     /**
@@ -66,23 +93,26 @@ export default {
      */
     if (this.$workbox) {
       this.$workbox.addEventListener("waiting", () => {
-      // this.showUpdateUI = true;
-      // notify the user before updating the app
-      this.$bvToast.toast(`Updating app to lastest version`, {
-        title: 'Updating',
-        solid: 'true',
-        variant: 'warning'
-        })
-      // pull the latest code
-      this.$workbox.messageSW({ type: "SKIP_WAITING" })
+        // this.showUpdateUI = true;
+        // notify the user before updating the app
+        this.$bvToast.toast(`Updating app to lastest version`, {
+          title: "Updating",
+          solid: "true",
+          variant: "warning",
+          autoHideDelay: 2000,
+        });
+        // pull the latest code
+        setTimeout(() => {
+          this.$workbox.messageSW({ type: "SKIP_WAITING" });
+        }, 2000);
       });
     }
-  // async accept() {
-  // this.showUpdateUI = false;
-  // this.$workbox.messageSW({ type: "SKIP_WAITING" })
-  // }
-  }
-}
+    // async accept() {
+    // this.showUpdateUI = false;
+    // this.$workbox.messageSW({ type: "SKIP_WAITING" })
+    // }
+  },
+};
 </script>
 <style scoped>
 .router-link-exact-active {
