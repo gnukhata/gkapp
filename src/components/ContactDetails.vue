ba6b7f12c9acbe9e5b0f2dcbdd051b74a90a16da
SaiKarthik <kskarthik@disroot.org>
2021-02-01 17:19:56 +0530

    code for updating customers/suppliers

--- src/components/ContactDetails.vue
+++ src/components/ContactDetails.vue
@@ -1,213 +1,163 @@
 <template>
-  <div class="main">
+  <b-form @submit.prevent="updateDetails">
     <b-overlay no-wrap blur :show="isLoading"></b-overlay>
-    <!-- Contact Details-->
-    <div v-if="noEdit" class="d-flex flex-row-reverse mb-2">
-      <b-button @click="noEdit = false" variant="warning"
-        ><b-icon icon="pencil-square"></b-icon> Edit Profile</b-button
-      >
-    </div>
-    <b-card bg-variant="light" border-variant="secondary">
+    <b-card
+      header="Details"
+      header-text-variant="light"
+      header-bg-variant="primary"
+      border-variant="secondary"
+    >
       <b-form-group
-        label-cols-lg="2"
-        label="Contact Details"
-        label-size="md"
-        label-class="font-weight-bold pt-0"
-        class="mb-0"
+        label="Name"
+        label-for="nested-street"
+        label-cols-sm="3"
+        label-align-sm="right"
       >
-        <b-form-group
-          label="Name:"
-          label-for="nested-street"
-          label-cols-sm="3"
-          label-align-sm="right"
-        >
-          <b-form-input
-            :value="details.custname"
-            id="nested-street"
-            :disabled="noEdit"
-          ></b-form-input>
-        </b-form-group>
+        <b-form-input
+          :value="details.custname"
+          id="nested-street"
+        ></b-form-input>
+      </b-form-group>
 
-        <b-form-group
-          label="Phone:"
-          label-for="nested-city"
-          label-cols-sm="3"
-          label-align-sm="right"
-        >
-          <b-form-input
-            :value="details.custphone"
-            id="nested-city"
-            :disabled="noEdit"
-          ></b-form-input>
-        </b-form-group>
+      <b-form-group
+        label="Phone"
+        label-for="nested-city"
+        label-cols-sm="3"
+        label-align-sm="right"
+      >
+        <b-form-input
+          :value="details.custphone"
+          id="nested-city"
+        ></b-form-input>
+      </b-form-group>
 
-        <b-form-group
-          label="Email:"
-          label-for="nested-state"
-          label-cols-sm="3"
-          label-align-sm="right"
-        >
-          <b-form-input
-            :value="details.custemail"
-            id="nested-state"
-            :disabled="noEdit"
-          ></b-form-input>
-        </b-form-group>
+      <b-form-group
+        label="Email:"
+        label-for="nested-state"
+        label-cols-sm="3"
+        label-align-sm="right"
+      >
+        <b-form-input :value="details.custemail"></b-form-input>
+      </b-form-group>
 
-        <b-form-group
-          label="Fax:"
-          label-for="nested-country"
-          label-cols-sm="3"
-          label-align-sm="right"
-        >
-          <b-form-input
-            :value="details.custfax"
-            id="nested-country"
-            :disabled="noEdit"
-          ></b-form-input>
-        </b-form-group>
+      <b-form-group
+        label="Fax:"
+        label-for="nested-country"
+        label-cols-sm="3"
+        label-align-sm="right"
+      >
+        <b-form-input :value="details.custfax"></b-form-input>
       </b-form-group>
     </b-card>
     <!-- Contact Address -->
-    <b-card bg-variant="light" border-variant="secondary" class="mt-2">
+    <b-card
+      header="Address"
+      header-text-variant="light"
+      header-bg-variant="primary"
+      border-variant="secondary"
+      class="mt-2"
+    >
       <b-form-group
-        label-cols-lg="2"
-        label="Address"
-        label-size="md"
-        label-class="font-weight-bold pt-0"
-        class="mb-0"
+        label="Street:"
+        label-for="nested-street"
+        label-cols-sm="3"
+        label-align-sm="right"
       >
-        <b-form-group
-          label="Street:"
-          label-for="nested-street"
-          label-cols-sm="3"
-          label-align-sm="right"
-        >
-          <b-form-input
-            :value="details.custaddr"
-            id="nested-street"
-            :disabled="noEdit"
-          ></b-form-input>
-        </b-form-group>
+        <b-form-input
+          :value="details.custaddr"
+          id="nested-street"
+        ></b-form-input>
+      </b-form-group>
 
-        <!-- <b-form-group
-          label="City:"
-          label-for="nested-city"
-          label-cols-sm="3"
-          label-align-sm="right"
-        >
-          <b-form-input id="nested-city"></b-form-input>
-        </b-form-group> -->
+      <b-form-group
+        label="State:"
+        label-for="nested-state"
+        label-cols-sm="3"
+        label-align-sm="right"
+      >
+        <b-form-select
+          :options="options.states"
+          :value="details.state"
+          v-model="details.state"
+        ></b-form-select>
+      </b-form-group>
 
-        <b-form-group
-          label="State:"
-          label-for="nested-state"
-          label-cols-sm="3"
-          label-align-sm="right"
-        >
-          <b-form-input
-            :value="details.state"
-            :disabled="noEdit"
-            id="nested-state"
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group
-          label="Pincode"
-          label-for="nested-country"
-          label-cols-sm="3"
-          label-align-sm="right"
-        >
-          <b-form-input
-            :value="details.pincode"
-            id="nested-country"
-            :disabled="noEdit"
-          ></b-form-input>
-        </b-form-group>
+      <b-form-group
+        label="Pincode"
+        label-for="nested-country"
+        label-cols-sm="3"
+        label-align-sm="right"
+      >
+        <b-form-input
+          :value="details.pincode"
+          id="nested-country"
+        ></b-form-input>
       </b-form-group>
     </b-card>
     <!-- Contact Bank details -->
-    <!-- {{ details }} -->
-    <b-card bg-variant="light" border-variant="secondary" class="mt-2">
+    <b-card
+      header="Financial Details"
+      header-bg-variant="primary"
+      header-text-variant="light"
+      border-variant="secondary"
+      class="mt-2"
+    >
       <b-form-group
-        label-cols-lg="2"
-        label="Financial Details"
-        label-size="md"
-        label-class="font-weight-bold pt-0"
-        class="mb-0"
+        label="PAN:"
+        label-for="nested-street"
+        label-cols-sm="3"
+        label-align-sm="right"
       >
-        <b-form-group
-          label="PAN:"
-          label-for="nested-street"
-          label-cols-sm="3"
-          label-align-sm="right"
-        >
-          <b-form-input
-            :value="details.custpan"
-            id="nested-street"
-            :disabled="noEdit"
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group
-          label="TAN:"
-          label-for="nested-city"
-          label-cols-sm="3"
-          label-align-sm="right"
-        >
-          <b-form-input
-            :value="details.custtan"
-            id="nested-city"
-            :disabled="noEdit"
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group
-          label="State:"
-          label-for="nested-state"
-          label-cols-sm="3"
-          label-align-sm="right"
-        >
-          <b-form-input
-            :value="details.state"
-            :disabled="noEdit"
-          ></b-form-input>
-        </b-form-group>
-
-        <!-- <b-form-group
-          label="Pincode"
-          label-for="nested-country"
-          label-cols-sm="3"
-          label-align-sm="right"
-        >
-          <b-form-input
-            :value="details.pincode"
-            id="nested-country"
-            :disabled="noEdit"
-          ></b-form-input>
-        </b-form-group> -->
+        <b-form-input
+          :value="details.custpan"
+          id="nested-street"
+        ></b-form-input>
       </b-form-group>
+
+      <b-form-group
+        label="TAN:"
+        label-for="nested-city"
+        label-cols-sm="3"
+        label-align-sm="right"
+      >
+        <b-form-input :value="details.custtan" id="nested-city"></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        label="GSTIN"
+        label-for="nested-state"
+        label-cols-sm="3"
+        label-align-sm="right"
+      >
+        <b-form-input
+          v-for="i in details.gstin"
+          :key="i"
+          :value="i"
+          class="m-1"
+          disabled
+        ></b-form-input>
+      </b-form-group>
+
+      <!-- <b-form-group
+        label="State:"
+        label-for="nested-state"
+        label-cols-sm="3"
+        label-align-sm="right"
+      >
+        <b-form-select
+          :options="options.states"
+          :value="details.state"
+          v-model="options.selectedState"
+        ></b-form-select>
+      </b-form-group> -->
     </b-card>
     <!-- Action Buttons -->
     <div class="mt-2 mb-3 d-flex flex-row-reverse">
-      <b-button
-        size="sm"
-        class="ml-2"
-        v-if="!noEdit"
-        @click="updateDetails"
-        variant="success"
+      <b-button type="submit" size="sm" class="ml-2" variant="success"
         ><b-icon icon="arrow-up-circle"></b-icon> Update Details</b-button
       >
-      <b-button
-        size="sm"
-        v-if="!noEdit"
-        class=""
-        @click="noEdit = true"
-        variant="danger"
-        ><b-icon icon="x-circle"></b-icon> Cancel</b-button
-      >
     </div>
-  </div>
+  </b-form>
 </template>
 
 <script>
@@ -219,9 +169,12 @@ export default {
   props: { customer: Object },
   data() {
     return {
-      details: null,
+      details: Array,
       isLoading: true,
-      noEdit: true,
+      options: {
+        states: [],
+        selectedState: {},
+      },
     };
   },
   computed: {
@@ -229,7 +182,7 @@ export default {
   },
   methods: {
     getDetails() {
-      let config = {
+      const config = {
         headers: {
           gktoken: this.authToken,
         },
@@ -242,21 +195,68 @@ export default {
         .then((res) => {
           this.details = res.data.gkresult;
           this.isLoading = false;
+          this.states();
         })
         .catch((err) => {
           console.log(err.message);
         });
     },
     /**
-     * Update user details
+     * Update customer/supplier details
      */
     updateDetails() {
-      this.$bvToast.toast("Profile Details Updated", {
-        title: "Success",
-        variant: "success",
-        solid: true,
-      });
-      this.noEdit = true;
+      delete this.details.statelist;
+      this.isLoading = true;
+      const config = {
+        headers: {
+          gktoken: this.authToken,
+        },
+      };
+      axios
+        .put(`${this.gkCoreUrl}/customersupplier`, this.details, config)
+        .then((res) => {
+          switch (res.data.gkstatus) {
+            case 0:
+              this.isLoading = false;
+              this.$bvToast.toast(
+                `${this.details.custname} Profile Details Updated`,
+                {
+                  title: "Success",
+                  variant: "success",
+                  solid: true,
+                }
+              );
+              break;
+            case 2:
+              this.isLoading = false;
+
+              this.$bvToast.toast("You have no permissions to modify details", {
+                variant: "danger",
+                solid: true,
+              });
+              break;
+          }
+        })
+        .catch((e) => {
+          this.$bvToast.toast(e.message, {
+            variant: "danger",
+            solid: true,
+          });
+        });
+    },
+    states() {
+      axios
+        .get(`${this.gkCoreUrl}/state`)
+        .then((res) => {
+          this.options.states = res.data.gkresult.map(
+            (item) => Object.values(item)[0]
+          );
+        })
+        .catch((e) => {
+          this.$bvToast.toast(e, {
+            variant: "danger",
+          });
+        });
     },
   },
   mounted() {
