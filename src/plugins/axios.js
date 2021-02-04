49b5daf243d4cb198144b2bee9aab9a3faa9e222
SaiKarthik <kskarthik@disroot.org>
2021-02-04 19:22:23 +0530

    set global gkcore url in axios config so that it need not be called on every axios request

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,9 +2,11 @@
 
 import Vue from 'vue';
 import axios from "axios";
+import store from '@/store'
 
+axios.defaults.baseURL = store.state.gkCoreUrl
+  ;
 // Full config:  https://github.com/axios/axios#request-config
-// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
@@ -17,11 +19,11 @@ let config = {
 const _axios = axios.create(config);
 
 _axios.interceptors.request.use(
-  function(config) {
+  function (config) {
     // Do something before request is sent
     return config;
   },
-  function(error) {
+  function (error) {
     // Do something with request error
     return Promise.reject(error);
   }
@@ -29,17 +31,17 @@ _axios.interceptors.request.use(
 
 // Add a response interceptor
 _axios.interceptors.response.use(
-  function(response) {
+  function (response) {
     // Do something with response data
     return response;
   },
-  function(error) {
+  function (error) {
     // Do something with response error
     return Promise.reject(error);
   }
 );
 
-Plugin.install = function(Vue) {
+Plugin.install = function (Vue) {
   Vue.axios = _axios;
   window.axios = _axios;
   Object.defineProperties(Vue.prototype, {
