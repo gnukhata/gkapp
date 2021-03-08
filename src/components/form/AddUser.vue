a86fb6e1d01c3087c9456fcc3feead2ba2d0b564
SaiKarthik <kskarthik@disroot.org>
2021-03-08 17:51:20 +0530

    enable adding godowns for newly created users

--- src/components/form/AddUser.vue
+++ src/components/form/AddUser.vue
@@ -12,13 +12,54 @@
         trim
       ></b-form-input>
       <b-form-invalid-feedback id="input-live-feedback">
-        Username must be greater than 3 characters
+        Username must be minimum 3 characters
       </b-form-invalid-feedback>
     </b-form-group>
     <b-form-group label="Select Role">
-      <b-form-select v-model="newUser.userrole" :options="roles" required>
+      <b-form-select
+        v-model="newUser.userrole"
+        :options="roles"
+        required
+        @change="getGodowns"
+      >
       </b-form-select>
     </b-form-group>
+    <b-table-simple
+      v-if="newUser.userrole === 3"
+      hover
+      small
+      caption-top
+      responsive
+    >
+      <caption>
+        Select Godowns
+      </caption>
+      <b-thead head-variant="dark">
+        <b-tr>
+          <b-th>Select</b-th>
+          <b-th>Name</b-th>
+          <b-th>State</b-th>
+          <b-th>Address</b-th>
+        </b-tr>
+      </b-thead>
+      <b-tbody>
+        <b-tr v-for="godown in allGodowns" :key="godown.goid">
+          <b-td>
+            <b-form-checkbox
+              value="accepted"
+              unchecked-value="not_accepted"
+              v-model="godown.checked"
+              switch
+            >
+            </b-form-checkbox>
+          </b-td>
+          <b-td>{{ godown.goname }}</b-td>
+          <b-td>{{ godown.state }}</b-td>
+          <b-td>{{ godown.goaddr }}</b-td>
+          <!-- <b-td>{{ godown.checked }}</b-td> -->
+        </b-tr>
+      </b-tbody>
+    </b-table-simple>
     <b-form-group label="Password">
       <b-form-input
         required
@@ -63,12 +104,14 @@ export default {
   data() {
     return {
       isLoading: false,
+      allGodowns: [],
       newUser: {
         username: "",
         userpassword: "",
         userrole: Number,
         userquestion: "",
         useranswer: "",
+        golist: [],
       },
       roles: [
         {
@@ -139,98 +182,128 @@ export default {
     // }
   },
   methods: {
+    /**
+     * get all godowns from current organisation
+     */
+    getGodowns() {
+      this.isLoading = true;
+      console.log("fetching godowns");
+      axios
+        .get(`${this.gkCoreUrl}/godown`, {
+          headers: { gktoken: this.authToken },
+        })
+        .then((r) => {
+          if (r.status == 200 && r.data.gkstatus == 0) {
+            let godowns = r.data.gkresult;
+            for (let i in godowns) {
+              godowns[i].checked = "not_accepted";
+            }
+            this.allGodowns = godowns;
+            this.isLoading = false;
+          }
+        })
+        .catch((e) => {
+          this.$bvToast.toast(
+            "Failed to fetch godown info, Please reload the page",
+            {
+              variant: "danger",
+              solid: true,
+            }
+          );
+          this.isLoading = false;
+        });
+    },
     addUser() {
       this.isLoading = true;
-      if (this.newUser.userrole !== 3) {
-        axios
-          .post(`${this.gkCoreUrl}/users`, this.newUser, {
-            headers: {
-              gktoken: this.authToken,
-            },
-          })
-          .then((r) => {
-            if (r.status == 200) {
-              switch (r.data.gkstatus) {
-                case 0:
-                  this.$bvToast.toast(
-                    `${this.newUser.username} created successfully`,
-                    {
-                      title: "Success",
-                      variant: "success",
-                      solid: true,
-                    }
-                  );
-                  // Add user created log to server
-                  const payload = {
-                    activity: `user ${this.newUser.username} created`,
-                  };
-                  axios.post(`${this.gkCoreUrl}/log`, payload, {
-                    headers: { gktoken: this.authToken },
-                  });
-                  // refresh users list in USerManagement.vue
-                  this.$emit("refreshUsers");
-                  this.isLoading = false;
-                  break;
-                case 1:
-                  this.$bvToast.toast(
-                    `Username ${this.newUser.username} already exists`,
-                    {
-                      variant: "danger",
-                      solid: true,
-                    }
-                  );
-                  this.isLoading = false;
-                  break;
-                case 2:
-                  this.$bvToast.toast("Unauthorised access", {
+      // If selected role is godown incharge, add selected godown id's to the submitted form
+      if (this.newUser.userrole == 3) {
+        let list = [];
+        for (let i in this.allGodowns) {
+          if (this.allGodowns[i].checked === "accepted") {
+            list.push(this.allGodowns[i].goid);
+          }
+        }
+        console.log(list);
+        this.newUser.golist = list;
+      }
+      axios
+        .post(`${this.gkCoreUrl}/users`, this.newUser, {
+          headers: {
+            gktoken: this.authToken,
+          },
+        })
+        .then((r) => {
+          if (r.status == 200) {
+            switch (r.data.gkstatus) {
+              case 0:
+                this.$bvToast.toast(
+                  `${this.newUser.username} created successfully`,
+                  {
+                    title: "Success",
+                    variant: "success",
+                    solid: true,
+                  }
+                );
+                // Add user created log to server
+                const payload = {
+                  activity: `user ${this.newUser.username} created`,
+                };
+                axios.post(`${this.gkCoreUrl}/log`, payload, {
+                  headers: { gktoken: this.authToken },
+                });
+                // refresh users list in USerManagement.vue
+                this.$emit("refreshUsers");
+                this.isLoading = false;
+                break;
+              case 1:
+                this.$bvToast.toast(
+                  `Username ${this.newUser.username} already exists`,
+                  {
                     variant: "danger",
                     solid: true,
-                  });
-                  this.isLoading = false;
-                  break;
-                case 3:
-                  this.$bvToast.toast("Connection Failed", {
-                    variant: "danger",
-                    solid: true,
-                  });
-                  this.isLoading = false;
-                  break;
-                case 4:
-                  this.$bvToast.toast("Bad Privilege", {
-                    variant: "danger",
-                    solid: true,
-                  });
-                  this.isLoading = false;
-                case 5:
-                  this.$bvToast.toast("Action Disallowed", {
-                    variant: "danger",
-                    solid: true,
-                  });
-                  this.isLoading = false;
-                  break;
-              }
-            } else {
-              this.$bvToast.toast(
-                "Connection failed with status code " + r.status,
-                {
+                  }
+                );
+                this.isLoading = false;
+                break;
+              case 2:
+                this.$bvToast.toast("Unauthorised access", {
                   variant: "danger",
                   solid: true,
-                }
-              );
-              this.isLoading = false;
+                });
+                this.isLoading = false;
+                break;
+              case 3:
+                this.$bvToast.toast("Connection Failed", {
+                  variant: "danger",
+                  solid: true,
+                });
+                this.isLoading = false;
+                break;
+              case 4:
+                this.$bvToast.toast("Bad Privilege", {
+                  variant: "danger",
+                  solid: true,
+                });
+                this.isLoading = false;
+              case 5:
+                this.$bvToast.toast("Action Disallowed", {
+                  variant: "danger",
+                  solid: true,
+                });
+                this.isLoading = false;
+                break;
             }
-          });
-      } else {
-        this.$bvToast.toast(
-          "Creating godown incharge role is not yet implemented",
-          {
-            title: "Alert",
-            variant: "warning",
-            solid: true,
+          } else {
+            this.$bvToast.toast(
+              "Connection failed with status code " + r.status,
+              {
+                variant: "danger",
+                solid: true,
+              }
+            );
+            this.isLoading = false;
           }
-        );
-        this.isLoading = false;
-      }
+        });
     },
     // getPasswordHint(pwdStrength) {
     //   const available = pwdStrength.contains.map((item) => item.message);
