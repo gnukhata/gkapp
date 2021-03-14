507afcc4bed742f7cff391b65b40d58b03b57593
SaiKarthik <kskarthik@disroot.org>
2021-03-14 15:07:51 +0530

    Change password module for all users
    * This module enables user to change password
    * Password validations:

---
+++ src/components/form/ChangePwd.vue
@@ -0,0 +1,186 @@
+<template>
+  <b-form @submit.prevent="validatePwd"
+    ><b-overlay :show="isLoading" blur no-wrap></b-overlay>
+    <b-form-group label="Current Password">
+      <b-form-input type="password" v-model.lazy="currentPwd" required>
+      </b-form-input>
+    </b-form-group>
+    <b-form-group label="New Password">
+      <b-form-input type="password" v-model="form.userpassword" required>
+      </b-form-input>
+    </b-form-group>
+    <b-form-group label="Confirm Password">
+      <b-form-input type="text" :state="matchingPwds" v-model="confirmPwd">
+      </b-form-input>
+      <b-form-invalid-feedback id="input-live-feedback">
+        Passwords do not match
+      </b-form-invalid-feedback>
+    </b-form-group>
+    <b-button
+      type="submit"
+      :disabled="!matchingPwds"
+      variant="success"
+      class="float-right"
+      ><b-icon icon="key"></b-icon> Change Password</b-button
+    >
+  </b-form>
+</template>
+
+<script>
+/*
+ /This module has functions to update current user's password
+ */
+import { mapState } from "vuex";
+export default {
+  name: "ChangePwd",
+  data() {
+    return {
+      isLoading: false,
+      currentPwd: "",
+      confirmPwd: "",
+      form: {
+        userid: Number,
+        userpassword: ""
+      }
+    };
+  },
+  computed: {
+    ...mapState(["userName", "gkCoreUrl", "authToken"]),
+    matchingPwds() {
+      if (this.form.userpassword === this.confirmPwd) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  methods: {
+    /* Get user's details from api */
+    getUserInfo() {
+      this.isLoading = true;
+      axios
+        .get(`${this.gkCoreUrl}/users?user=single`, {
+          headers: {
+            gktoken: this.authToken
+          }
+        })
+        .then(r => {
+          if (r.status == 200) {
+            let info = r.data.gkresult;
+            switch (r.data.gkstatus) {
+              case 0:
+                this.form.userid = info.userid;
+                this.isLoading = false;
+                break;
+              default:
+                this.$bvToast.toast("Request failed", {
+                  title: `status code ${r.data.gkstatus}`,
+                  variant: "danger",
+                  solid: true
+                });
+                this.isLoading = false;
+            }
+          }
+        })
+        .catch(e => {
+          console.log(e);
+          this.isLoading = false;
+        });
+    },
+    /* Check if user's password is valid */
+    validatePwd() {
+      this.isLoading = true;
+      axios
+        .post(
+          `${this.gkCoreUrl}/users?userloginstatus`,
+          {
+            username: this.userName,
+            userpassword: this.currentPwd
+          },
+          { headers: { gktoken: this.authToken } }
+        )
+        .then(r => {
+          if (r.status == 200) {
+            switch (r.data.gkstatus) {
+              case 0:
+                this.changePwd();
+                break;
+              case 4:
+                this.$bvToast.toast("Invalid Password, Please try again", {
+                  solid: true,
+                  variant: "danger"
+                });
+                this.isLoading = false;
+                break;
+              default:
+                this.$bvToast.toast(`Failed with gkstatus ${r.data.gkstatus}`, {
+                  solid: true,
+                  variant: "danger"
+                });
+                this.isLoading = false;
+            }
+          }
+        })
+        .catch(e => {
+          this.$bvToast.toast(e.message, {
+            solid: true,
+            variant: "danger"
+          });
+          this.isLoading = false;
+        });
+    },
+    /* Proceed to changing user's password on successful validation */
+    changePwd() {
+      axios
+        .put(
+          `${this.gkCoreUrl}/users`,
+          {
+            userid: this.form.userid,
+            username: this.userName,
+            userpassword: this.form.userpassword
+          },
+          {
+            headers: { gktoken: this.authToken }
+          }
+        )
+        .then(r => {
+          if (r.status == 200) {
+            switch (r.data.gkstatus) {
+              case 0:
+                this.$bvToast.toast(
+                  `Password Change Successful for ${this.userName}`,
+                  {
+                    title: "Success",
+                    variant: "success",
+                    solid: true
+                  }
+                );
+                this.isLoading = false;
+                break;
+              default:
+                this.$bvToast.toast(
+                  `Failed to update password for ${this.userName}`,
+                  {
+                    title: `gkstatus ${r.data.gkstatus}`,
+                    variant: "danger",
+                    solid: true
+                  }
+                );
+                this.isLoading = false;
+            }
+          }
+        })
+        .catch(e => {
+          this.$bvToast.toast(e.message, {
+            variant: "danger",
+            solid: true
+          });
+          this.isLoading = false;
+        });
+    }
+  },
+  mounted() {
+    this.getUserInfo();
+  }
+};
+</script>
