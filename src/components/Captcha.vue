de3f6f8c4450a267c385a983e3843a75e71ce748
SaiKarthik <kskarthik@disroot.org>
2021-03-18 00:47:01 +0530

    UI for captcha component

---
+++ src/components/Captcha.vue
@@ -0,0 +1,93 @@
+<template>
+  <b-form-row>
+    <canvas
+      aria-label="Question audio button"
+      title="Audio question"
+      :width="width"
+      :height="height"
+      id="captchaCanvas"
+      style="border: 1px solid black; background-color: antiquewhite"
+      class="ml-2"
+    ></canvas>
+    <b-icon
+      class="ml-3 mt-2"
+      @click="audioCaptcha"
+      role="button"
+      icon="volume-up-fill"
+      variant="dark"
+      title="Audio Captcha"
+      aria-label="audio question button"
+      scale="1.5x"
+    ></b-icon>
+    <b-icon
+      class="ml-2 mt-2"
+      @click="newCaptcha"
+      role="button"
+      icon="arrow-clockwise"
+      variant="dark"
+      title="New captcha"
+      aria-label="new captcha button"
+    ></b-icon>
+  </b-form-row>
+</template>
+
+<script>
+export default {
+  name: "Captcha",
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    },
+    // Canvas width (optional)
+    width: {
+      default: 90
+    },
+    // canvas height (optional)
+    height: {
+      default: 30
+    }
+  },
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+    /*
+     / This method randomly generates simple addition sums
+     / in range of 0-10
+     */
+    newCaptcha() {
+      let a = Math.floor(Math.random() * 11);
+      let b = Math.floor(Math.random() * 11);
+      this.text = `${a} + ${b}`;
+      this.value = a + b;
+      const canvas = document.getElementById("captchaCanvas");
+      const ctx = canvas.getContext("2d");
+      ctx.font = "20px Arial";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.clearRect(0, 0, this.width, this.height);
+      ctx.fillText(`${this.text} = `, this.width / 2, this.height / 2);
+    },
+    /* This method converts the question to audio
+    / for the pupose of users who require accessibility features
+    /*/
+    audioCaptcha() {
+      if ("speechSynthesis" in window) {
+        const msg = new SpeechSynthesisUtterance();
+        msg.text = this.question;
+        window.speechSynthesis.speak(msg + "=");
+      } else {
+        this.$bvModal.msgBoxOk(
+          "Your browser does not support speech synthesis"
+        );
+      }
+    }
+  },
+  mounted() {
+    this.newCaptcha();
+  }
+};
+</script>
