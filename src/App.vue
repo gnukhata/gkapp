77f1c98462c65bc778a0ff2d11c3887c465eceb9
SaiKarthik <kskarthik@disroot.org>
2021-05-04 11:52:46 +0530

    move menu items from user area to sidebar

--- src/App.vue
+++ src/App.vue
@@ -60,25 +60,24 @@
                 {{ userName }}
               </template>
               <!-- admin only options -->
-              <template v-if="userRole == -1">
-                <b-dropdown-item to="/orgprofile"
-                  ><b-icon icon="building"></b-icon> Organisation
-                  Profile</b-dropdown-item
-                >
-                <b-dropdown-item to="/uom"
-                  ><b-icon icon="thermometer"></b-icon> Unit of measurement
-                </b-dropdown-item>
-                <b-dropdown-item to="/user_management"
-                  ><b-icon icon="people"></b-icon> Manage Users</b-dropdown-item
-                >
-                <b-dropdown-item to="/logs"
-                  ><b-icon icon="server"></b-icon> Audit Logs</b-dropdown-item
-                >
-                <b-dropdown-item to="/closebooks"
-                  ><b-icon icon="journals"></b-icon> Close Books / Roll
-                  Over</b-dropdown-item
-                >
-              </template>
+              <!-- <template v-if="userRole == -1">
+                   <b-dropdown-item to="/orgprofile"
+                   ><b-icon icon="building"></b-icon> Organisation
+                   Profile</b-dropdown-item
+                   >
+                   <b-dropdown-item to="/uom"
+                   ><b-icon icon="thermometer"></b-icon> Unit of measurement
+                   </b-dropdown-item>
+                   <b-dropdown-item to="/user_management"
+                   ><b-icon icon="people"></b-icon> Manage Users</b-dropdown-item
+                   >
+                   <b-dropdown-item to="/logs"
+                   ><b-icon icon="server"></b-icon> Audit Logs</b-dropdown-item
+                   >
+                   <b-dropdown-item to="/closebooks"
+                   ><b-icon icon="journals"></b-icon> Close Books / Roll
+                   Over</b-dropdown-item
+                   >
               <b-dropdown-item
                 :to="{
                   name: 'Billwise',
@@ -87,6 +86,7 @@
                 ><b-icon icon="clipboard"></b-icon> Adjust
                 Bills</b-dropdown-item
               >
+                   </template> -->
               <b-dropdown-item v-b-modal.change-pwd
                 ><b-icon icon="key"></b-icon> Change Password
               </b-dropdown-item>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,16 +1,48 @@
 <template>
   <section>
-    <b-button size="sm" class="ml-2" variant="light" v-b-toggle.sidebar-1>
-      <b-icon icon="box-arrow-right"></b-icon>
+    <b-button
+      size="sm"
+      class="ml-2"
+      variant="light"
+      title="Open Sidebar"
+      :lazy="true"
+      v-b-toggle.sidebar-1
+    >
+      <b-icon icon="arrow-bar-right"></b-icon>
     </b-button>
-    <b-sidebar v-model="opened" id="sidebar-1" shadow>
-      <div>
-        General
-      </div>
-      <ul>
-        <li>Company Info</li>
-        <li>Unit of Measurement</li>
-      </ul>
+    <b-sidebar v-model="opened" id="sidebar-1" title="MENU" shadow>
+      <template>
+        <b-dropdown-item to="/orgprofile"
+          ><b-icon icon="building"></b-icon> Organisation
+          Profile</b-dropdown-item
+        >
+        <b-dropdown-item to="/uom"
+          ><b-icon icon="thermometer"></b-icon> Unit of measurement
+        </b-dropdown-item>
+        <b-dropdown-item to="/user_management"
+          ><b-icon icon="people"></b-icon> Manage Users</b-dropdown-item
+        >
+        <b-dropdown-item to="/logs"
+          ><b-icon icon="server"></b-icon> Audit Logs</b-dropdown-item
+        >
+        <b-dropdown-item
+          :to="{
+            name: 'Billwise',
+            params: { custType: 3, custName: '-1' },
+          }"
+          ><b-icon icon="clipboard"></b-icon> Adjust Bills</b-dropdown-item
+        >
+        <b-dropdown-item to="/closebooks"
+          ><b-icon icon="journals"></b-icon> Close Books / Roll
+          Over</b-dropdown-item
+        >
+        <b-dropdown-item to="/godowns"
+          ><b-icon icon="building"></b-icon> Godowns</b-dropdown-item
+        >
+        <b-dropdown-item to="/costcenter"
+          ><b-icon icon="cash-stack"></b-icon> Cost Center</b-dropdown-item
+        >
+      </template>
     </b-sidebar>
   </section>
 </template>
