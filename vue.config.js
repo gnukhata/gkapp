8c1589efe12da3167c79f663373a726673b40298
SaiKarthik <kskarthik@disroot.org>
2021-05-17 22:34:45 +0530

    try fixing env var

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? process.env.GKAPP_URL : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? `${process.env.GKAPP_URL}` : '/',
     lintOnSave: 'warning',
     // Disable js source maps for production build
     productionSourceMap: false
