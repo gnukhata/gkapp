ee91ee8cf2e56b582d01ff4c5c80e945a2a66f29
SaiKarthik <kskarthik@disroot.org>
2021-06-24 15:02:49 +0530

    Option to set custom app url by setting up env variable
    - Update related docs on howto with examples.
    - Update the CI file with new feature!

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: '/gkapp/',
+    publicPath: process.env.GKAPP_URL !== undefined ? process.env.GKAPP_URL : '/',
     lintOnSave: 'warning',
     // Disable js source maps for production build
     productionSourceMap: false
