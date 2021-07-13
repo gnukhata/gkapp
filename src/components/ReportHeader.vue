c88b66d5e1ca76f0546b0a9e59bd41bd515b60f2
Sai Karthik <kskarthik@disroot.org>
2021-07-13 23:16:29 +0530

    report header component which displays org details in print view

---
+++ src/components/ReportHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="d-none d-print-block">
+    <div class="text-center">
+      <h3>{{ orgName }}</h3>
+    </div>
+    <slot> </slot>
+    <div class="text-center">
+      <small>
+        Printed by User: <b>{{ userName }}</b> <i>on</i> {{ new Date() }}
+      </small>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+  name: 'ReportHeader',
+  computed: {
+    ...mapState(['orgName', 'userName']),
+  },
+};
+</script>
