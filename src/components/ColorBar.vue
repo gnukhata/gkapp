127b80e2b54bef3ed10aeaba760d8b09c2beddb0
Sai Karthik <kskarthik@disroot.org>
2021-09-07 15:03:00 +0530

    fix: hide colorbar in print view

--- src/components/ColorBar.vue
+++ src/components/ColorBar.vue
@@ -1,48 +1,66 @@
 <template>
-    <div class="d-flex flex-row justify-content-left">
-        <div class="m-1" v-for="(style, index) in colorVersion" :key="index" :style="style"></div>
-    </div>
+  <div class="d-flex flex-row justify-content-left d-print-none">
+    <div
+      class="m-1"
+      v-for="(style, index) in colorVersion"
+      :key="index"
+      :style="style"
+    ></div>
+  </div>
 </template>
 
 <script>
-import {version} from '../../package'
+import { version } from '../../package';
 export default {
-    name: 'ColorBar',
-    data() {
-        return {
+  name: 'ColorBar',
+  data() {
+    return {
       // color codes for numbers
-        // number: {
-        //     zero: 'violet',
-        //     one: 'Indigo',
-        //     two: 'blue',
-        //     three: 'green',
-        //     four: 'yellow',
-        //     five: 'orange',
-        //     six: 'red',
-        //     seven: 'black',
-        //     eight: 'maroon',
-        //     nine: 'coral'
-        //     },
-            colors: [
-            'violet','Indigo','blue',
-            'green', 'yellow', 'orange', 
-            'red','black','maroon','coral'
-            ],
-        colorVersion: []
-        }
+      // number: {
+      //     zero: 'violet',
+      //     one: 'Indigo',
+      //     two: 'blue',
+      //     three: 'green',
+      //     four: 'yellow',
+      //     five: 'orange',
+      //     six: 'red',
+      //     seven: 'black',
+      //     eight: 'maroon',
+      //     nine: 'coral'
+      //     },
+      colors: [
+        'violet',
+        'Indigo',
+        'blue',
+        'green',
+        'yellow',
+        'orange',
+        'red',
+        'black',
+        'maroon',
+        'coral',
+      ],
+      colorVersion: [],
+    };
+  },
+  methods: {
+    ver() {
+      const projectVersion = version.split('.');
+      this.colorVersion = [
+        `background-color: ${
+          this.colors[projectVersion[0]]
+        }; height: 3px; width: 55%`,
+        `background-color: ${
+          this.colors[projectVersion[1]]
+        }; height: 3px; width: 35%`,
+        `background-color: ${
+          this.colors[projectVersion[2]]
+        }; height: 3px; width: 15%`,
+      ];
     },
-    methods: {
-        ver() {
-            const projectVersion = version.split('.')
-            this.colorVersion = [
-                `background-color: ${this.colors[projectVersion[0]]}; height: 3px; width: 55%`,
-                `background-color: ${this.colors[projectVersion[1]]}; height: 3px; width: 35%`,
-                `background-color: ${this.colors[projectVersion[2]]}; height: 3px; width: 15%`
-            ]
-        }
-    },
-    mounted() {
-        this.ver()
-    }
-}
+  },
+  mounted() {
+    this.ver();
+  },
+};
 </script>
