dd399ed4c8fa24ae563ca89ff9fbccf13e7a0573
Sai Karthik <kskarthik@disroot.org>
2021-10-14 20:47:26 +0530

    feat(password): New features!
    - passwords are now hashed with SHA512 digest before sent to server for validation
    - added feature (icon) to reveal password
    - added placeholder prop

--- src/components/Password.vue
+++ src/components/Password.vue
@@ -4,19 +4,32 @@
     :invalid-feedback="passwordFeedback"
     :valid-feedback="passwordFeedback"
   >
-    <b-form-input
-      :size="size"
-      type="password"
-      v-model.lazy="userPassword"
-      :state="isPasswordValid"
-      required
-    >
-    </b-form-input>
+    <b-input-group>
+      <b-form-input
+        :size="size"
+        :type="field.type"
+        v-model.lazy="userPassword"
+        :state="isPasswordValid"
+        required
+        aria-label="Password input"
+        :placeholder="placeholder"
+      >
+      </b-form-input>
+      <b-input-group-append
+        ><b-button
+          @click="toggleVisibility"
+          :size="size"
+          class="ml-1"
+          variant="light"
+          ><b-icon :icon="field.icon"></b-icon></b-button
+      ></b-input-group-append>
+    </b-input-group>
   </b-form-group>
 </template>
 
 <script>
 import passwordStrength from 'check-password-strength';
+// import sha512 from 'crypto-js/sha512';
 
 export default {
   name: 'Password',
@@ -26,29 +39,54 @@ export default {
   },
   props: {
     size: String,
+    passwordHint: {
+      value: Boolean,
+      required: false,
+      default: true,
+    },
+    placeholder: {
+      value: String,
+      required: false,
+      default: 'Placeholder',
+    },
   },
   data() {
     return {
       userPassword: '',
+      field: {
+        type: 'password',
+        icon: 'eye-fill',
+      },
       pwdFieldTypes: ['lowercase', 'uppercase', 'symbol', 'number'],
     };
   },
   methods: {
+    toggleVisibility() {
+      if (this.field.type === 'password') {
+        this.field.type = 'text';
+        this.field.icon = 'eye-slash-fill';
+      } else {
+        this.field.type = 'password';
+        this.field.icon = 'eye-fill';
+      }
+    },
     getPasswordHint(pwdStrength) {
-      const available = pwdStrength.contains.map((item) => item.message);
-      let hint = this.pwdFieldTypes
-        .filter((item) => !available.includes(item))
-        .reduce((prev, cur) => {
-          return `${prev} ${cur},`;
-        }, '');
-      hint = hint.substring(0, hint.length - 1);
-      if (available.length < 4) {
-        hint = `Recommended atleast 1 ${hint}`;
+      if (this.passwordHint) {
+        const available = pwdStrength.contains.map((item) => item.message);
+        let hint = this.pwdFieldTypes
+          .filter((item) => !available.includes(item))
+          .reduce((prev, cur) => {
+            return `${prev} ${cur},`;
+          }, '');
+        hint = hint.substring(0, hint.length - 1);
+        if (available.length < 4) {
+          hint = `Recommended atleast 1 ${hint}`;
+        }
+        if (pwdStrength.length < 8) {
+          hint += ' & minimum 8 characters long';
+        }
+        return hint;
       }
-      if (pwdStrength.length < 8) {
-        hint += ' & minimum 8 characters long';
-      }
-      return hint;
     },
   },
   computed: {
@@ -67,7 +105,8 @@ export default {
         switch (this.pwdStrength.value) {
           case 'Strong':
             text = 'Your Password is Strong!';
-            this.$emit('change', this.userPassword);
+            // v-model will emit hashed password digest
+            this.$emit('change', this.hashedPassword(this.userPassword));
             break;
           case 'Medium':
             text = this.getPasswordHint(this.pwdStrength);
@@ -82,7 +121,8 @@ export default {
               text = 'is-success';
             }
         }
-        this.$emit('change', this.userPassword);
+        // v-model will emit hashed password digest
+        this.$emit('change', this.hashedPassword(this.userPassword));
       }
       return text;
     },
