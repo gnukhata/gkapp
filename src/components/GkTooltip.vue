c5ac51ba162b13bc306e2c68b8d650502af41f0a
Sai Karthik <kskarthik@disroot.org>
2021-11-06 23:48:08 +0530

    feat: gktooltip component

---
+++ src/components/GkTooltip.vue
@@ -0,0 +1,54 @@
+<template>
+  <b-icon
+    v-b-popover.hover="helpContent"
+    tabindex="0"
+    role="link"
+    :variant="variant"
+    :icon="icon"
+  ></b-icon>
+</template>
+
+<script>
+export default {
+  name: 'GkTooltip',
+  props: {
+    helpTitle: {
+      type: String,
+      default: 'Help',
+      note: 'title of help popover (optional)',
+    },
+    helpBody: {
+      type: String,
+      default: 'Fill text here',
+      required: true,
+      note: 'body of help popover (required)',
+    },
+    icon: {
+      type: String,
+      default: 'question-circle',
+      required: false,
+      note: 'Icon for tooltip',
+    },
+    variant: {
+      type: String,
+      default: 'dark',
+      required: false,
+      note: 'Tooltip variant',
+    },
+  },
+  computed: {
+    helpContent() {
+      let o = {
+        html: true,
+        placement: 'auto',
+        variant: 'dark',
+        content: this.helpBody,
+      };
+      if (this.helpTitle !== null) {
+        o['title'] = this.helpTitle;
+      }
+      return o;
+    },
+  },
+};
+</script>
