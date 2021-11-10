06a53dcefcfffed5656610f6d1c66571aa58e26e
Sai Karthik <kskarthik@disroot.org>
2021-11-10 13:14:40 +0530

    feat(report header): added show prop to display in all screens

--- src/components/ReportHeader.vue
+++ src/components/ReportHeader.vue
@@ -1,11 +1,11 @@
 <template>
-  <section class="d-none d-print-block">
+  <section :class="show ? '' : 'd-none d-print-block'">
     <div class="text-center">
       <h3>{{ orgName }}</h3>
     </div>
     <slot> </slot>
     <div class="text-center">
-      <small>
+      <small v-if="!show">
         Printed by User: <b>{{ userName }}</b> <i>on</i> {{ new Date() }}
       </small>
     </div>
@@ -15,6 +15,12 @@
 <script>
 import { mapState } from 'vuex';
 export default {
+  props: {
+    show: {
+      value: [String, Boolean],
+      default: false,
+    },
+  },
   name: 'ReportHeader',
   computed: {
     ...mapState(['orgName', 'userName']),
