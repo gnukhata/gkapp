6806c061bd8601a4d8e40d3dae8c9fac9863d792
Sai Karthik <kskarthik@disroot.org>
2022-02-20 23:33:41 +0530

    feat: <gk-toolbar>

---
+++ src/components/GkToolbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="mb-2 shadow-sm bg-light d-flex flex-row justify-content-end">
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'GkToolbar',
+};
+</script>
