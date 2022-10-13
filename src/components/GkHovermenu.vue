6ecbdc7afebc243893bad128a4c05ab6f0c2dc3d
Sai Karthik <kskarthik@disroot.org>
2022-10-13 19:11:09 +0530

    feat(GkHovermenu): hover menu component to use with filters

---
+++ src/components/GkHovermenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="d-print-none">
+    <!-- trigger button -->
+    <b-button :title="title" variant="link" id="select">
+      <b-icon :icon="icon" class="mt-1"></b-icon>
+    </b-button>
+    <!-- Filters -->
+    <b-popover
+      class="bg-light d-print-none"
+      target="select"
+      triggers="click"
+      placement="bottom"
+    >
+      <slot></slot>
+    </b-popover>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GkHovermenu',
+  props: {
+    icon: {
+      type: String,
+      default: 'funnel',
+    },
+    title: {
+      type: String,
+      default: 'Filter',
+    },
+  },
+};
+</script>
+
+<style></style>
