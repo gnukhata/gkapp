7f148ec28e78091352e6e54d8db668456f915ac5
Sai Karthik <kskarthik@disroot.org>
2022-10-13 19:12:48 +0530

    feat(GkToolbar): Add left and center slots

--- src/components/GkToolbar.vue
+++ src/components/GkToolbar.vue
@@ -1,6 +1,17 @@
 <template>
-  <section class="mb-2 shadow-sm bg-light d-flex flex-row justify-content-end">
-    <slot></slot>
+  <section
+    class="d-print-none container-fluid mb-2 bg-light d-flex justify-content-between"
+  >
+    <div class="row">
+      <slot name="left"></slot>
+    </div>
+    <div class="row">
+      <slot name="center"></slot>
+    </div>
+    <!-- this slot aligns to right & is the default slot -->
+    <div class="row">
+      <slot></slot>
+    </div>
   </section>
 </template>
 
