daf1021eee7807036118557873c5159852c7541e
Sai Karthik <kskarthik@disroot.org>
2023-03-13 23:33:23 +0530

    fix(dashboard): handle -ve balance for bank/cash bal
    Closes #299

--- src/views/dashboard/Balance.vue
+++ src/views/dashboard/Balance.vue
@@ -1,51 +1,61 @@
-    <template>
-      <div v-if="show" class="mb-3 d-flex flex-row justify-content-center">
-        <div class="rounded  m-2 p-3 bg-warning">
-          <div class="row">
-            <div class="col">
-              <b-icon icon="building" class="mr-1"></b-icon>Bank Balance:
-              <b>{{ gk_currency(currentBalance.bankaccdata[0].bankbalance) }}</b>
-            </div>
-          </div>
-        </div>
-        <div class="rounded m-2 p-3 bg-success text-light">
-          <div class="row">
-            <div class="col">
-              <b-icon icon="cash" class="mr-1"></b-icon>Cash Balance:
-              <b>{{ gk_currency(currentBalance.cashaccdata[0].cashbalance) }}</b>
-            </div>
-          </div>
+<template>
+  <div v-if="show" class="mb-3 d-flex flex-row justify-content-center">
+    <div class="rounded  m-2 p-3 bg-warning">
+      <div class="row">
+        <div class="col">
+          <b-icon icon="building" class="mr-1"></b-icon>Bank Balance:
+          <b v-if="currentBalance.bankaccdata[0].baltype == 'Cr'">
+            - {{ gk_currency(currentBalance.bankaccdata[0].bankbalance) }}</b
+          >
+          <b v-else>
+            {{ gk_currency(currentBalance.bankaccdata[0].bankbalance) }}</b
+          >
         </div>
       </div>
-    </template>
+    </div>
+    <div class="rounded m-2 p-3 bg-success text-light">
+      <div class="row">
+        <div class="col">
+          <b-icon icon="cash" class="mr-1"></b-icon>Cash Balance:
+          <b v-if="currentBalance.cashaccdata[0].baltype == 'Cr'"
+            >- {{ gk_currency(currentBalance.cashaccdata[0].cashbalance) }}</b
+          >
+          <b v-else>{{
+            gk_currency(currentBalance.cashaccdata[0].cashbalance)
+          }}</b>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
-    <script>
-    import axios from 'axios';
-    export default {
-      name: 'Balance',
-      data() {
-        return {
-          currentBalance: [],
-          show: false,
-        };
-      },
-      methods: {
-        getBalances() {
-          axios
-            .get('/dashboard?type=cashbankaccountdata')
-            .then((r) => {
-              if (r.status == 200) {
-                this.currentBalance = r.data;
-                this.show = true;
-              }
-            })
-            .catch((e) => {
-              console.log(e);
-            });
-        },
-      },
-      created() {
-        this.getBalances();
-      },
+<script>
+import axios from 'axios';
+export default {
+  name: 'Balance',
+  data() {
+    return {
+      currentBalance: [],
+      show: false,
     };
-    </script>
+  },
+  methods: {
+    getBalances() {
+      axios
+        .get('/dashboard?type=cashbankaccountdata')
+        .then((r) => {
+          if (r.status == 200) {
+            this.currentBalance = r.data;
+            this.show = true;
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+  created() {
+    this.getBalances();
+  },
+};
+</script>
