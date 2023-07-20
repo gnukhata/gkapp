be3d10496d72519686d5a9a8dd9023c71ec449b4
Sai Karthik <kskarthik@disroot.org>
2023-07-20 22:01:22 +0530

    feat: rework of print letter head

--- src/components/ReportHeader.vue
+++ src/components/ReportHeader.vue
@@ -1,16 +1,51 @@
 <template>
   <section :class="show ? '' : 'd-none d-print-block'">
-    <div class="text-center bg-light mb-2">
-      <h3>{{ orgName }}</h3>
-      <span v-if="gstin" class="mr-1 font-weight-bold">GSTIN: </span>
-      <span v-if="gstin" class="text-monospace">{{ gstin }}</span>
+    <!-- org details card, appears while printing only -->
+    <div class="d-flex flex-row justify-content-between m-2 bg-light">
+      <div class="d-none d-print-block p-1">
+        <img
+          :src="orgImg"
+          width="30"
+          height="30"
+          class="rounded d-inline-block align-top"
+          alt="Logo"
+        />
+        <h5 class="d-inline-block ml-1">{{ orgAddress.orgname || 'N/A' }}</h5>
+        <br />
+        <small><b>Address: </b> {{ orgAddress.orgaddr || 'N/A' }}</small>
+        <br />
+        <small><b>City: </b>{{ orgAddress.orgcity || 'N/A' }}</small> <br />
+      </div>
+      <div>
+        <small><b>Contact No: </b> {{ orgAddress.orgtelno || 'N/A' }}</small>
+        <br />
+        <small v-if="orgAddress.gstin != null"
+          ><b>GSTIN: </b>
+          {{ Object.values(orgAddress.gstin)[0] || 'N/A' }}</small
+        ><br />
+        <small><b>State: </b>{{ orgAddress.orgstate || 'N/A' }}</small> <br />
+      </div>
+      <div>
+        <small><b>Date:</b> {{ dateReverse(currentDate()) }}</small
+        ><br />
+        <small
+          ><b>Time:</b> {{ new Date().toTimeString().split(' ')[0] }}
+          {{ new Date().toTimeString().split(' ')[1] }}</small
+        ><br />
+        <small><b>User:</b> {{ userName }}</small>
+      </div>
     </div>
+    <!-- <div class="text-center bg-light mb-2"> -->
+    <!--   <h3>{{ orgName }}</h3> -->
+    <!--   <span v-if="gstin" class="mr-1 font-weight-bold">GSTIN: </span> -->
+    <!--   <span v-if="gstin" class="text-monospace">{{ gstin }}</span> -->
+    <!-- </div> -->
     <slot> </slot>
-    <div class="text-center">
-      <small v-if="!show">
-        Printed by User: <b>{{ userName }}</b> <i>on</i> {{ new Date() }}
-      </small>
-    </div>
+    <!-- <div class="text-center"> -->
+    <!--   <small v-if="!show"> -->
+    <!--     Printed by User: <b>{{ userName }}</b> <i>on</i> {{ new Date() }} -->
+    <!--   </small> -->
+    <!-- </div> -->
   </section>
 </template>
 
@@ -25,7 +60,7 @@ export default {
   },
   name: 'ReportHeader',
   computed: {
-    ...mapState(['orgName', 'userName', 'orgAddress']),
+    ...mapState(['orgName', 'orgImg', 'userName', 'orgAddress']),
     gstin: (self) => (self?.orgAddress.gstin ? self.orgAddress.gstin[0] : ''),
   },
 };
--- src/components/workflow/profile/Transaction.vue
+++ src/components/workflow/profile/Transaction.vue
@@ -1,30 +1,6 @@
 <template>
   <!--add custom data here  -->
   <div>
-    <!-- org details card, appears while printing only -->
-    <div class="d-flex flex-row-reverse">
-      <div class="d-none d-print-block w-50 border mb-2 p-1 mr-1">
-        <img
-          :src="orgImg"
-          width="30"
-          height="30"
-          class="rounded d-inline-block align-top"
-          alt="Logo"
-        />
-        <h5 class="d-inline-block ml-1">{{ orgAddress.orgname || 'N/A' }}</h5>
-        <br />
-        <small><b>Address: </b> {{ orgAddress.orgaddr || 'N/A' }}</small>
-        <br />
-        <small><b>City: </b>{{ orgAddress.orgcity || 'N/A' }}</small> <br />
-        <small><b>State: </b>{{ orgAddress.orgstate || 'N/A' }}</small> <br />
-        <small><b>Contact No: </b> {{ orgAddress.orgtelno || 'N/A' }}</small>
-        <br />
-        <small v-if="orgAddress.gstin != null"
-          ><b>GSTIN: </b>
-          {{ Object.values(orgAddress.gstin)[0] || 'N/A' }}</small
-        >
-      </div>
-    </div>
     <component v-bind:is="name" :pdata="pdata" :id="id" :onUpdate="onUpdate">
     </component>
   </div>
