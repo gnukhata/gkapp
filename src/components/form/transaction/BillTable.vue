a58770a9550835ec9388b78f24d66fd11bf4f543
priyanka@1992 <priyanka.bagade@accionlabs.com>
2024-01-17 19:32:39 +0530

    fix:  IssueUI>>Workflow>>Transaction>>Invoice>>In Create Invoice Buyer Details name, Item selection, Place to supply From Godown values are unable to select by using Automation script:revert back last commit

--- src/components/form/transaction/BillTable.vue
+++ src/components/form/transaction/BillTable.vue
@@ -95,21 +95,19 @@
           <span>{{ data.label }}</span>
         </template>
         <template #cell(product)="data">
-          <b-form-select
+        <v-select
             v-if="form[data.item.index] && !disabled.product"
-            @input="onBillItemSelect(form[data.item.index].product, data.item.index)"
+            @input="
+              onBillItemSelect(form[data.item.index].product, data.item.index)
+            "
             :options="options.products"
+            label="name"
             v-model="form[data.item.index].product"
             placeholder="Select Product/Service"
+            :selectable="checkProductValidity"
             :disabled="disabled.product"
-            value-field="name" 
-            text-field="name"
-            :required="true"
           >
-            <template #first>
-              <b-form-select-option :value="null" disabled>Select Product/Service</b-form-select-option>
-            </template>
-            <template v-slot:option="{ option }">
+            <template v-slot:option="option">
               <div v-if="options.productData[option.id].gsflag !== 19">
                 {{ option.name }}
                 <div class="text-small" v-if="options.stock[option.id] > 0">
@@ -118,10 +116,10 @@
                 <div v-else>({{ options.stock[option.id] || 0 }})</div>
               </div>
             </template>
-          </b-form-select>
-          <span v-else>
-            {{ data.value.name || (form[data.item.index]?.product?.name || '') }}
-          </span>
+          </v-select>
+          <span v-else>{{
+            data.value.name || form[data.item.index]?.product.name || ''
+          }}</span>
         </template>
 
 
