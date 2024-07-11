87c95fe06f99862c0b9ee8ca8dd5d4bbc4d113f8
Bady <bady@disroot.org>
2024-07-11 10:27:28 +0530

    fix: boolean environment variables are taken as string

--- src/js/config/gkConfig.js
+++ src/js/config/gkConfig.js
@@ -1,14 +1,14 @@
 export default {
   gkcore_url: process.env.VUE_APP_GKCORE_URL,
-  fetch_latest_version: process.env.VUE_APP_FETCH_LATEST_VERSION,
-  login_captcha: process.env.VUE_APP_LOGIN_CAPTCHA,
+  fetch_latest_version: process.env.VUE_APP_FETCH_LATEST_VERSION === 'true',
+  login_captcha: process.env.VUE_APP_LOGIN_CAPTCHA === 'true',
   login_banner: {
-    show: process.env.VUE_APP_LOGIN_BANNER_SHOW,
+    show: process.env.VUE_APP_LOGIN_BANNER_SHOW === 'true',
     variant: process.env.VUE_APP_LOGIN_BANNER_VARIANT,
     content: process.env.VUE_APP_LOGIN_BANNER_CONTENT,
   },
   sidebar_banner: {
-    show: process.env.VUE_APP_SIDEBAR_BANNER_SHOW,
+    show: process.env.VUE_APP_SIDEBAR_BANNER_SHOW === 'true',
     variant: process.env.VUE_APP_SIDEBAR_BANNER_VARIANT,
     content: process.env.VUE_APP_SIDEBAR_BANNER_CONTENT,
   },
