068e9bd0bd31363bf8866293309087414bf2f140
priyanka@1992 <priyanka.bagade@accionlabs.com>
2024-07-18 15:03:00 +0530

    code changes fix

--- src/components/form/transaction/BillTable.vue
+++ src/components/form/transaction/BillTable.vue
@@ -1302,10 +1302,10 @@ export default {
               }
               const rateValue = this.crdrnote ? item.taxableamount : rate;
               if (this.crdrnote) {
-                if(!item.dcValue && (this.purposeSelectedValue && this.purposeSelectedValue != 18)) {
-                  item.taxable = 0;
-                } else {
+                if((this.purposeSelectedValue && this.purposeSelectedValue != 18)) {
                   item.taxable = parseFloat(item.dcValue || 0) * qty;
+                } else {
+                  item.taxable = parseFloat((rateValue * qty).toFixed(2));
                 }
               } else {
                 item.taxable = parseFloat((rateValue * qty - item.discount.amount).toFixed(2));
