02c5d012f2e8c82dd9ec4e5fce2b1b1dee5c7a5b
Bady <bady@disroot.org>
2024-08-09 15:33:29 +0530

    feat: update total amount split-up in total-table

--- src/components/form/transaction/TotalTable.vue
+++ src/components/form/transaction/TotalTable.vue
@@ -1,79 +1,76 @@
 <template>
-  <b-table-simple responsive>
-    <b-thead>
-      <b-tr>
-        <b-th v-translate>Total</b-th>
-        <b-th :style="{ minWidth: '70px' }" class="text-right text-truncate"
-          >₹</b-th
-        >
-      </b-tr>
-    </b-thead>
-    <b-tbody>
-      <template v-if="isIndia">
-        <b-tr v-if="(gstFlag || vatFlag) && config.taxable">
-          <b-td v-translate>Taxable Amount</b-td>
-          <b-td class="text-right">{{ form.taxable || '-' }}</b-td>
+  <div>
+    <b-form-checkbox
+      switch
+      inline
+      size="sm"
+      v-model="form.roundFlag"
+      v-if="config.roundOff"
+      class="mb-3 float-right"
+    >
+      <translate>Round Off Total Value</translate>
+    </b-form-checkbox>
+    <b-table-simple responsive>
+      <b-tbody>
+        <b-tr>
+          <b-td v-translate>Total</b-td>
+          <b-td :style="{ minWidth: '70px' }" class="text-right text-truncate">
+            {{ totalPrice }}
+          </b-td>
         </b-tr>
-        <b-tr v-if="gstFlag && cgstFlag && config.igst">
-          <b-td>CGST</b-td>
-          <b-td class="text-right">{{ form.igst / 2 || '-' }}</b-td>
+        <b-tr v-if="config.discount">
+          <b-td v-translate>Discount</b-td>
+          <b-td class="text-right">{{ totalDiscount }}</b-td>
         </b-tr>
-        <b-tr v-if="gstFlag && cgstFlag && config.igst">
-          <b-td>SGST</b-td>
-          <b-td class="text-right">{{ form.igst / 2 || '-' }}</b-td>
+        <template v-if="isIndia">
+          <b-tr v-if="(gstFlag || vatFlag) && config.taxable">
+            <b-td v-translate>Taxable Amount</b-td>
+            <b-td class="text-right">{{ form.taxable || '-' }}</b-td>
+          </b-tr>
+          <b-tr v-if="gstFlag && cgstFlag && config.igst">
+            <b-td>CGST</b-td>
+            <b-td class="text-right">{{ form.igst / 2 || '-' }}</b-td>
+          </b-tr>
+          <b-tr v-if="gstFlag && cgstFlag && config.igst">
+            <b-td>SGST</b-td>
+            <b-td class="text-right">{{ form.igst / 2 || '-' }}</b-td>
+          </b-tr>
+          <b-tr v-if="gstFlag && !cgstFlag && config.igst">
+            <b-td>IGST</b-td>
+            <b-td class="text-right">{{ form.igst || '-' }}</b-td>
+          </b-tr>
+          <b-tr v-if="gstFlag && config.cess">
+            <b-td>CESS</b-td>
+            <b-td class="text-right">{{ form.cess || '-' }}</b-td>
+          </b-tr>
+          <b-tr v-if="vatFlag && config.vat">
+            <b-td>VAT</b-td>
+            <b-td class="text-right">{{ form.vat || '-' }}</b-td>
+          </b-tr>
+        </template>
+      </b-tbody>
+      <b-tfoot>
+        <b-tr v-if="config.value">
+          <b-th>
+            <span class="float-left">{{ transactionName }}</span>
+          </b-th>
+          <b-th class="text-right">{{ form.amount || '-' }}</b-th>
         </b-tr>
-        <b-tr v-if="gstFlag && !cgstFlag && config.igst">
-          <b-td>IGST</b-td>
-          <b-td class="text-right">{{ form.igst || '-' }}</b-td>
+        <b-tr v-if="form.roundFlag && config.roundOff">
+          <b-th>
+            {{ transactionName }} <span v-translate>(Rounded Off)</span>
+          </b-th>
+          <b-th class="text-right">{{ form.rounded || '-' }}</b-th>
         </b-tr>
-        <b-tr v-if="gstFlag && config.cess">
-          <b-td>CESS</b-td>
-          <b-td class="text-right">{{ form.cess || '-' }}</b-td>
+        <b-tr v-if="config.valueText">
+          <b-th>
+            {{ transactionName }} <span v-translate>(in words)</span>
+          </b-th>
+          <b-th class="text-right"> {{ totalText }}</b-th>
         </b-tr>
-        <b-tr v-if="vatFlag && config.vat">
-          <b-td>VAT</b-td>
-          <b-td class="text-right">{{ form.vat || '-' }}</b-td>
-        </b-tr>
-      </template>
-      <b-tr v-if="config.discount">
-        <b-td v-translate>Discount</b-td>
-        <b-td class="text-right">{{ form.discount || '-' }}</b-td>
-      </b-tr>
-      <b-tr v-if="config.value">
-        <b-td>
-          <span class="float-left">{{ transactionName }}</span>
-        </b-td>
-        <b-td class="text-right">{{ form.amount || '-' }}</b-td>
-      </b-tr>
-      <b-tr v-if="form.roundFlag && config.roundOff">
-        <b-td
-          >{{ transactionName }} <span v-translate> (Rounded Off) </span>
-        </b-td>
-        <b-td class="text-right">{{ form.rounded || '-' }}</b-td>
-      </b-tr>
-      <b-tr v-if="config.valueText">
-        <b-td
-          >{{ transactionName }} <span v-translate> (in words) </span>
-        </b-td>
-        <b-td class="text-right"> {{ totalText }}</b-td>
-      </b-tr>
-      <b-tr>
-        <b-td>
-          <b-form-checkbox
-            switch
-            inline
-            size="sm"
-            v-model="form.roundFlag"
-            v-if="config.roundOff"
-            class="ml-2 float-left"
-          >
-            <translate> Round Off Total Value </translate>
-          </b-form-checkbox>
-        </b-td>
-        <b-td></b-td>
-      </b-tr>
-    </b-tbody>
-  </b-table-simple>
+      </b-tfoot>
+    </b-table-simple>
+  </div>
 </template>
 
 <script>
@@ -143,6 +140,14 @@ export default {
     },
   },
   computed: {
+    totalPrice: (self) => (
+      (parseFloat(self.form.taxable) + parseFloat(self.form.discount)
+    ).toFixed(2)),
+    totalDiscount: (self) => (
+      parseFloat(self.form.discount)
+        ? `-${parseFloat(self.form.discount).toFixed(2)}`
+        : '0.00'
+    ),
     totalText: (self) => {
       let total = self.form.amount;
       let text = '';
