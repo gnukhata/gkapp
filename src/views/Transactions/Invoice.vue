65196fb9fdcdb6a252fae1eae05a82bca330f38c
Bady <bady@disroot.org>
2024-09-11 23:53:13 +0530

    fix: party form object not updated reactively in invoice view

--- src/views/Transactions/Invoice.vue
+++ src/views/Transactions/Invoice.vue
@@ -613,7 +613,7 @@ export default {
             this.options.partyDetails = payload;
 
             this.setBankDetails();
-            Object.assign(this.form.party, payload.data);
+            this.form.party = Object.assign({}, this.form.party, payload.data);
 
             this.updateCounter.ship++;
             Object.assign(this.form.inv.taxState, payload.data.state);
