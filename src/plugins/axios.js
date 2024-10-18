edb32944728b0058eca6aa870c6b9c323e751c73
Bady <bady@disroot.org>
2024-10-18 09:57:54 +0530

    refactor: simplify instance method in axios plugin

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,46 +3,9 @@
 import Vue from 'vue';
 import axios from "axios";
 
-const _axios = axios.create();
-
-_axios.interceptors.request.use(
-  function (config) {
-    // Do something before request is sent
-    return config;
-  },
-  function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
-);
-
-// Add a response interceptor
-_axios.interceptors.response.use(
-  function (response) {
-    // Do something with response data
-    return response;
-  },
-  function (error) {
-    // Do something with response error
-    return Promise.reject(error);
-  }
-);
-
 Plugin.install = function (Vue) {
-  Vue.axios = _axios;
-  window.axios = _axios;
-  Object.defineProperties(Vue.prototype, {
-    axios: {
-      get() {
-        return _axios;
-      }
-    },
-    $axios: {
-      get() {
-        return _axios;
-      }
-    },
-  });
+  const _axios = axios.create();
+  Vue.prototype.$axios = _axios;
 };
 
 Vue.use(Plugin)
