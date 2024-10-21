da00813e42881f630145cec0fed0efc081a200a4
Bady <bady@disroot.org>
2024-10-21 10:09:37 +0530

    feat: use axios response interceptor to show toasts on errors
    This helps to avoid manually handling axios errors.

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -5,6 +5,30 @@ import axios from "axios";
 
 Plugin.install = function (Vue) {
   const _axios = axios.create();
+
+  _axios.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      const vm = new Vue();
+      let message;
+      const { status } = error.response ?? {};
+      if (status >= 400 && status < 500) {
+        message = 'Client side error occurred, please try again later';
+      } else if (status >= 500 && status < 600) {
+        message = 'Server side error occurred, please try again later';
+      } else {
+        message = 'Some error occurred, please try again later';
+      }
+      vm.$bvToast.toast(message, {
+        title: 'Error',
+        variant: 'danger',
+      }) ;
+      return Promise.reject(error);
+    }
+ );
+
   Vue.prototype.$axios = _axios;
 };
 
