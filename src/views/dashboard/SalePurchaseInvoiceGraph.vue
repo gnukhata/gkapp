994f4f57cee5394c2b8416c95e45a2edd8edbf2a
Kannan V M <kannan@disroot.org>
2025-01-18 15:31:35 +0530

    feat: merge sale - purchase invoice bar-graph to single ine graph

--- src/views/dashboard/SalePurchaseInvoiceGraph.vue
+++ src/views/dashboard/SalePurchaseInvoiceGraph.vue
@@ -1,122 +1,65 @@
 <template>
-  <b-card-group
-    deck
-    class="mt-5"
-  >
-    <!-- Sale Invoices -->
-    <b-card no-body>
-      <bar
-        :chart-options="chartOptions"
-        :chart-data="saleChartData"
-        :chart-id="chartId"
-        :dataset-id-key="datasetIdKey"
-        :plugins="plugins"
-        :css-classes="cssClasses"
-        :styles="styles"
-        :width="width"
-        :height="height"
-      />
-    </b-card>
-    <!-- Purchase Invoices -->
-    <b-card no-body>
-      <bar
-        :chart-options="chartOptions"
-        :chart-data="purchaseChartData"
-        :chart-id="chartId"
-        :dataset-id-key="datasetIdKey"
-        :plugins="plugins"
-        :css-classes="cssClasses"
-        :styles="styles"
-        :width="width"
-        :height="height"
-      />
-    </b-card>
-  </b-card-group>
+  <!-- Sale Invoices -->
+  <line-chart
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+  />
 </template>
 
 <script>
-// TODO vue3: migration
-import { Bar } from 'vue-chartjs/legacy';
+import { Line as LineChart } from 'vue-chartjs/legacy';
 import {
   Chart as ChartJS,
   Title,
   Tooltip,
   Legend,
-  BarElement,
-  CategoryScale,
+  LineElement,
   LinearScale,
+  CategoryScale,
+  PointElement,
 } from 'chart.js';
 
 ChartJS.register(
   Title,
   Tooltip,
   Legend,
-  BarElement,
+  LineElement,
+  LinearScale,
   CategoryScale,
-  LinearScale
+  PointElement
 );
 
 export default {
   name: 'SalePurchaseInvoiceGraph',
-  components: { Bar },
+  components: { LineChart },
   props: {
     info: {
       type: Object,
       default: () => {},
     },
-    chartId: {
-      type: String,
-      default: 'bar-chart',
-    },
-    datasetIdKey: {
-      type: String,
-      default: 'label',
-    },
-    width: {
-      type: Number,
-      default: 400,
-    },
-    height: {
-      type: Number,
-      default: 200,
-    },
-    cssClasses: {
-      default: '',
-      type: String,
-    },
-    styles: {
-      type: Object,
-      default: () => {},
-    },
-    plugins: {
-      type: Array,
-      default: () => [],
-    },
   },
   data() {
     return {
-      purchaseChartData: {
+      chartData: {
         labels: this.info.purchase.map(item => item.month),
         datasets: [
           {
             label: 'Purchase Invoices',
             backgroundColor: '#f87979',
             data: this.info.purchase.map(item => item.balance),
+            tension: 0.3,
           },
-        ],
-      },
-      saleChartData: {
-        labels: this.info.sale.map(item => item.month),
-        datasets: [
           {
             label: 'Sale Invoices',
             backgroundColor: '#f87979',
             data: this.info.sale.map(item => item.balance),
+            tension: 0.3,
           },
         ],
       },
       chartOptions: {
         responsive: true,
+        maintainAspectRatio: false,
       },
     };
   },
