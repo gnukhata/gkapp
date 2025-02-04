48eef301cdbd4ee4859db5f4fce5ca1528a2a023
Bady <bady@disroot.org>
2025-02-04 16:11:37 +0530

    feat: update organisation export data page design

--- src/views/data/Export.vue
+++ src/views/data/Export.vue
@@ -1,39 +1,37 @@
 <template>
-  <div class="m-2">
-    <b-card
-      class="gkcard mx-auto"
-      :header="this.$gettext('Export Company Data')"
-      header-bg-variant="dark"
-      header-text-variant="light"
+  <section class="container-fluid mt-2">
+    <b-form
+      required
+      @submit.prevent="check"
     >
-      <b-card-body>
-        <b-form
-          required
-          @submit.prevent="check"
-        >
+      <b-card no-body>
+        <template #header>
+          <h5 class="my-2">
+            Export Data
+          </h5>
+        </template>
+        <b-card-body>
           <div v-translate>
-            This menu will enable you to export organisation data, that is, all
-            its accounts and records in these accounts in two formats:
-
+            You can export your organisation data in two formats:
             <ul class="mt-2">
               <li>Microsoft Excel Open XML Spreadsheet (.xslx) [legacy]</li>
               <li>Json (.json)</li>
             </ul>
             Click on any of the available "Export" buttons to start the export.
-            Please do not reload or close the window while the export process is
-            on. A download window will pop up when export is done. You can open
-            or save the file.
+            Please do not reload or close the window while the export process
+            is going on. A download window will pop up when export is done.
+            You can open or save the file.
           </div>
           <br>
           <!-- Export buttons -->
-          <b-button-group>
+          <b-button-group size="sm">
             <gk-file-download
               :url="
                 `/export/xlsx?yearstart=${this.yearStart}&yearend=${this.yearEnd}`
               "
               :file-name="`GNUKhata-DataExport`"
               name="Export (Legacy)"
-              variant="dark"
+              variant="success"
               :add-date="true"
               :add-time-stamp="true"
               class="mr-1"
@@ -43,8 +41,8 @@
               :url="`/export/json`"
               file-extn="json"
               icon="file-earmark-code"
-              name="Export (Json)"
-              variant="dark"
+              name="Export (JSON)"
+              variant="success"
               :add-date="true"
               file-suffix="Export"
               :add-time-stamp="true"
@@ -68,10 +66,10 @@
               accounts & vouchers data only
             </translate>
           </b-alert>
-        </b-form>
-      </b-card-body>
-    </b-card>
-  </div>
+        </b-card-body>
+      </b-card>
+    </b-form>
+  </section>
 </template>
 
 <script>
