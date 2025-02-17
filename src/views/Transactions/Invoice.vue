2011da760aa0bc8ddcdc694ac69b1853f49ea2f7
Bady <bady@disroot.org>
2025-02-17 10:03:40 +0530

    feat: update transaction form buttons for better consistency

--- src/views/Transactions/Invoice.vue
+++ src/views/Transactions/Invoice.vue
@@ -200,39 +200,7 @@
         :parent-data="form.attachments"
       />
       <hr>
-      <div class="float-right">
-        <b-button
-          class="m-1"
-          size="sm"
-          variant="danger"
-          @click.prevent="$router.go(-1)"
-        >
-          <b-icon
-            aria-hidden="true"
-            class="align-middle mr-1"
-            icon="arrow-left"
-          />
-          <span
-            class="align-middle"
-            v-translate
-          >Back</span>
-        </b-button>
-        <b-button
-          class="m-1"
-          size="sm"
-          variant="warning"
-          @click.prevent="resetForm"
-        >
-          <b-icon
-            aria-hidden="true"
-            class="align-middle mr-1"
-            icon="arrow-repeat"
-          />
-          <span
-            class="align-middle"
-            v-translate
-          >Reset</span>
-        </b-button>
+      <div>
         <b-button
           id="inv-submit"
           :disabled="isInvDateValid === false"
@@ -241,22 +209,28 @@
           class="m-1"
           variant="success"
         >
-          <span>
-            <b-spinner
-              v-if="isLoading"
-              small
-            />
-            <b-icon
-              v-else
-              aria-hidden="true"
-              class="align-middle mr-1"
-              icon="plus-square"
-            />
-            <span
-              class="align-middle"
-              v-translate
-            >Create</span>
-          </span>
+          <b-spinner
+            v-if="isLoading"
+            class="mr-1"
+            small
+          />
+          Save
+        </b-button>
+        <b-button
+          class="m-1"
+          size="sm"
+          variant="dark"
+          @click.prevent="resetForm"
+        >
+          Reset
+        </b-button>
+        <b-button
+          class="m-1"
+          size="sm"
+          variant="dark"
+          @click.prevent="$router.go(-1)"
+        >
+          Cancel
         </b-button>
       </div>
       <div class="clearfix" />
