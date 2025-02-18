fe855640a9007b80e95077d9a0b97f3dfd18612c
Kannan V M <kannan@disroot.org>
2025-02-18 21:28:16 +0530

    enhancement: update table style and improve pagination for uoms and godowns
    This will,
    - Increase width for list.
    - Add "..." in pagination if there is more than 4 pages.
    - Stop showing pagination if it has only one page.

--- src/views/Godowns.vue
+++ src/views/Godowns.vue
@@ -3,7 +3,7 @@
     <b-row>
       <!-- List Section -->
       <b-col
-        md="3"
+        md="4"
         class="mb-4 mb-md-0 pr-md-0"
         v-if="!showDetail"
       >
@@ -53,11 +53,12 @@
           </b-table>
           <b-pagination
             class="mt-3"
+            v-if="items.length > perPage"
             v-model="currentPage"
             :total-rows="items.length"
             :per-page="perPage"
             align="center"
-            limit="2"
+            limit="4"
           />
         </b-card>
       </b-col>
