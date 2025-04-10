a26e8c8b6e4a6c7dbf7d7ba4309a53906dc85995
Bady <bady@disroot.org>
2025-04-10 12:38:03 +0530

    feat: set BASE_URL from .env file in playwright config
    If BASE_URL is not set in the .env file, it will use a fallback value.

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 /**
  * Read environment variables from file.
@@ -27,7 +30,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: process.env.BASE_URL || 'http://localhost:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
