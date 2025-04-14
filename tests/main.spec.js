ad1d9829414479888bdc1fe3f3d6817f9e011008
Bady <bady@disroot.org>
2025-04-14 09:40:31 +0530

    feat: add test for creating invoices with multiple products

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,8 +1,39 @@
 import { expect } from '@playwright/test';
 import test from './fixtures/organisation';
+import BusinessItem from './models/business-item';
+import Invoice from './models/invoice';
 
 // eslint-disable-next-line no-unused-vars
 test('basic test', async ({ organisation, page }) => {
   await page.goto('/#/dashboard');
   await expect(page.locator('#usermenu')).toContainText('gkadmin');
+});
+
+// eslint-disable-next-line no-unused-vars
+test('invoice test', async ({ organisation, page, baseURL }) => {
+  const businessItem = new BusinessItem(page, baseURL);
+  await businessItem.create({
+    name: 'TestProduct',
+    costPrice: '900',
+    salePrice: '1100',
+    discount: '100',
+    stockQty: '100',
+    hsn: '6213',
+    gst: '12',
+  });
+  await businessItem.create({
+    name: 'TestProduct2',
+    costPrice: '900',
+    salePrice: '1100',
+    discount: '100',
+    stockQty: '100',
+    hsn: '6213',
+    gst: '12',
+  });
+  const invoice = new Invoice(page, baseURL);
+  await invoice.create({
+    invoiceType: 'sale',
+    contactName: 'Retail Customer (Default)',
+    items: ['TestProduct', 'TestProduct2'],
+  });
 });
