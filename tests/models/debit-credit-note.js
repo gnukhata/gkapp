0a4e8696a264701952be42f3a9fe36800babf4a7
Bady <bady@disroot.org>
2025-04-16 08:12:10 +0530

    feat: add page object model for debit/credit note with create method

---
+++ tests/models/debit-credit-note.js
@@ -0,0 +1,43 @@
+export default class DebitCreditNote {
+  constructor(page, baseURL) {
+    this.page = page;
+    this.baseURL = baseURL;
+  }
+
+  async create({
+    invoiceId,
+    invoiceType,
+    noteType,
+    purpose = 'price',
+    value,
+  }) {
+    const purposeOptions = {
+      price: 'Adjust Price / Discount',
+      qty: 'Adjust Qty / Return Goods',
+      deficiency: 'Deficiency in services',
+      correction: 'Correction in Invoice',
+      pos: 'Change in POS',
+      finalization: 'Finalization of Provisional assessment',
+      others: 'Others',
+    };
+    await this.page.goto(`${this.baseURL}#/dc-note?type=${invoiceType}`);
+    await this.page.locator('label').filter(
+      { hasText: noteType === 'debit' ? 'Debit Note' : 'Credit Note' }
+    ).click();
+    await this.page.getByPlaceholder('Choose an Invoice').click();
+    await this.page.locator('#input-8-2 > ul li', { hasText: `${invoiceId},` }).click();
+    await this.page.getByLabel('Purpose').selectOption({ label: purposeOptions[purpose] });
+    let inputColumnIndex = 3;
+    if (purpose === 'qty') {
+      inputColumnIndex = 2;
+    }
+    await this.page.getByRole('cell')
+      .nth(inputColumnIndex)
+      .locator('div > input[type="number"]')
+      .fill(value);
+    await this.page.getByRole('button', { name: 'Create' }).click();
+    await this.page.getByRole('button', { name: 'OK' }).click();
+    await this.page.getByRole('button', { name: 'Close' }).click();
+    await this.page.locator('#button-wrapper > .btn-danger').click();
+  }
+}
