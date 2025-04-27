8452c840c26d9286ce5b2316e569e4b67d698878
kannan <kannan@disroot.org>
2025-04-27 17:17:44 +0530

    refactor: move global setup to configs

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  globalSetup: './tests/global-setup.js',
+  globalSetup: './tests/configs/global-setup.js',
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL || 'http://localhost:8080',
