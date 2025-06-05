4dc12e004d98a2d4cdcc2393a033c828f81cf7e7
Bady <bady@disroot.org>
2025-06-05 08:28:41 +0530

    fix: update form submit button locator in invoice page object model

--- tests/models/invoice.js
+++ tests/models/invoice.js
@@ -39,7 +39,7 @@ export default class Invoice {
       await this.page.getByRole('textbox', { name: 'Branch' }).fill(branch);
     }
     const invoiceNumber = await this.page.getByRole('textbox', { name: 'Inv. #' }).inputValue();
-    await this.page.getByRole('button', { name: 'Create', exact: true }).click();
+    await this.page.getByRole('button', { name: 'Save', exact: true }).click();
     await this.page.getByRole('button', { name: 'OK' }).click();
     await this.page.getByRole('button', { name: 'Close' }).click();
     await this.page.locator('#button-wrapper > .btn-danger').click();
