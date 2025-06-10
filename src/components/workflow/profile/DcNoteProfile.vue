571df1787b538e9553ab52c88ab487c1ba374fe0
Bady <bady@disroot.org>
2025-06-10 22:32:46 +0530

    fix: show HSN/SAC summary title in transaction profiles only if GST is enabled

--- src/components/workflow/profile/DcNoteProfile.vue
+++ src/components/workflow/profile/DcNoteProfile.vue
@@ -154,10 +154,12 @@
         </p>
       </b-col>
     </b-row>
-    <div class="hsn-details mt-4">
+    <div
+      v-if="isGstEnabled"
+      class="hsn-details mt-4"
+    >
       <h6>HSN / SAC Summary</h6>
       <b-table-lite
-        v-if="isGstEnabled"
         :items="dcNote.dcItems"
         :fields="hsnFields"
         bordered
