3e769f4bd0fc543e434249bf31bdc2122f62ace6
Bady <bady@disroot.org>
2025-06-23 15:05:59 +0530

    feat: sort vouchers in descending order in payment component

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -144,6 +144,7 @@ export default {
               });
             });
           });
+          this.transactions.sort((a, b) => b.voucherId - a.voucherId);
         })
         .finally(() => {
           this.isLoading = false;
